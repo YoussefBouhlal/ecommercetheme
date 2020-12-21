46718842e6787920aa00e9083e594145642276e5
Youssef Bouhlal <pro.youssef.bouhlal@gmail.com>
2020-12-21 11:40:28 +0100

    add functions file

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Theme Functions
+ * 
+ * @package Ucef
+ */
+
+if ( ! defined( 'UCEF_DIR_PATH' ) )
+{
+    define( 'UCEF_DIR_PATH', untrailingslashit( get_template_directory() ) );
+}
+if ( ! defined( 'UCEF_DIR_URI' ) )
+{
+    define( 'UCEF_DIR_URI', untrailingslashit( get_template_directory_URI() ) );
+}
+
+
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) :
+    require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+endif;
+
+if ( class_exists( 'Ucef\\Init' ) ) :
+    Ucef\Init::register_services();
+endif;
