349c6f7670eb5bc9cc48354a16fef791314bddd2
Youssef Bouhlal <pro.youssef.bouhlal@gmail.com>
2021-01-02 12:12:17 +0100

    style shop page WOO

--- inc/Woocommerce/ContentProduct.php
+++ inc/Woocommerce/ContentProduct.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * custom woocommerce contetn product actions.
+ * custom woocommerce content product actions.
  *
  * @package Ucef
  */
@@ -15,43 +15,49 @@ class ContentProduct
      */
     public function register()
     {
-        // add_filter('woocommerce_output_related_products_args', array( $this, 'thumbnail_columns' ) );
+        // add the container
+        add_action( 'woocommerce_before_main_content', array( $this, 'before_main_content'), 10 );
 
-        add_action( 'woocommerce_before_main_content', array( $this, 'before_main_content') );
+        // remove the page title
+        add_filter( 'woocommerce_show_page_title', array( $this, 'hide_page_title' ), 10 ); 
+        
+        // remove the count of showing result
+        remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
 
-        add_action( 'woocommerce_before_shop_loop', array( $this, 'thumbnail_columns'), 40 );
-        add_action( 'woocommerce_after_shop_loop', array( $this, 'thumbnail_columns_after'), 40 );
+        // remove the catalog of ordring
+        remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
 
-        add_action( 'woocommerce_before_shop_loop_item', array( $this, 'before_shop_loop_item'), 10, 0 );
-        add_action( 'woocommerce_before_shop_loop_item_title', array( $this, 'add_befor_price'), 10, 0 );
-        add_action( 'woocommerce_after_shop_loop_item_title', array( $this, 'add_after_price'), 10, 0 );
+        // remove the sale tag
+        remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
+
+        // remove add to cart button
+        remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+
+        // 
+        add_action( 'woocommerce_after_shop_loop_item', array( $this, 'add_wishlist_btn' ) );
+
+        // remove side bar
+        remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar' );
+
+
     }
 
     function before_main_content( )
     {
-        echo '<div class="colomns-4">';
+        echo '<div class="container">';
     }
-    function thumbnail_columns( )
+    function hide_page_title( )
     {
-        echo '<div class="colomns-4">';
+        return;
     }
-    function thumbnail_columns_after( )
+    function add_wishlist_btn()
     {
-        echo '</div>';
-    }
-
-    function before_shop_loop_item()
-    {
-        echo 'First One';
-    }
-
-    function add_befor_price()
-    {
-        echo 'hiiiii';
+        echo '<div class="loop-product__wishlist">';
+            echo '<button class="loop-product__wishlist__btn btn bg-white">';
+                echo '<div class="loop-product__wishlist__image">';
+                    svg('heart');
+                echo '</div> </button> </div>';
     }
     
-    function add_after_price()
-    {
-        echo 'hellooo';
-    }
+
 }
